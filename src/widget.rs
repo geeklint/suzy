255ff2a2384cbaf72325004736881c88aef07396
Violet <violet@geeklint.com>
2019-12-01 19:09:35 -0500

    add basic documentation to widget.rs

--- src/widget.rs
+++ src/widget.rs
@@ -7,6 +7,7 @@ use crate::graphics::{Canvas, CanvasRenderer};
 use crate::interaction::{InteractionReceiver, Touch};
 
 
+/// A version of Rect where each dimension will trigger watching functions
 #[derive(Default)]
 pub struct WidgetRect {
     x: Watched<Dim>,
@@ -20,11 +21,15 @@ impl<'a> Rect for WidgetRect {
     fn y_mut<F: FnOnce(&mut Dim)>(&mut self, f: F) { (f)( &mut self.y ) }
 }
 
+/// This will get passed to a widget's initializer. It provides functions to
+/// watch values for changes and run code when those values change
 pub struct WidgetInit<'a, T: WidgetData> {
     watcher: &'a mut WatcherMeta<WidgetInternal<T>>,
 }
 
 impl<T: WidgetData + 'static> WidgetInit<'_, T> {
+    /// Register a simple watch which will get re-run whenever a value it
+    /// references changes.
     pub fn watch<F>(&mut self, func: F)
         where F: Fn(&mut T, &WidgetRect) + 'static
     {
@@ -33,6 +38,8 @@ impl<T: WidgetData + 'static> WidgetInit<'_, T> {
         });
     }
 
+    /// Register a function which draws to the widget's canvas and updates
+    /// whenever referenced values change.
     pub fn draw<F>(self, func: F)
         where F: Fn(&mut T, &WidgetRect, &mut Canvas) + 'static
     {
@@ -41,6 +48,8 @@ impl<T: WidgetData + 'static> WidgetInit<'_, T> {
         });
     }
 
+    /// Register a function which draws to the widget's canvas that renders
+    /// on-top of it's children.
     pub fn draw_after<F>(self, func: F)
         where F: Fn(&mut T, &WidgetRect, &mut Canvas) + 'static
     {
@@ -50,6 +59,8 @@ impl<T: WidgetData + 'static> WidgetInit<'_, T> {
         });
     }
 
+    /// Register functions which draw to the widget's canvas before and after
+    /// the children respectively.
     pub fn draw_before_and_after<F, G>(
         self, before: F, after: G)
         where F: Fn(&mut T, &WidgetRect, &mut Canvas) + 'static,
@@ -66,11 +77,19 @@ impl<T: WidgetData + 'static> WidgetInit<'_, T> {
     }
 }
 
+/// This trait should be implemented for the types you provide as data for
+/// Widget implementations.
 pub trait WidgetData: Sized + InteractionReceiver {
+    /// This method provides a convient place to register functions which
+    /// watch values and update parts of your widget when they change
     fn init(init: &mut WidgetInit<Self>);
 
+    /// Custom widgets must define a way to iterate over their children
+    /// if they want those children to be visible
     fn children<'a>(&'a self) -> Vec<WidgetProxy<'a>>;
 
+    /// Custom widgets must define a way to iterate over their children
+    /// if they want those children to be visible
     fn children_mut<'a>(&'a mut self) -> Vec<WidgetProxyMut<'a>>;
 }
 
@@ -88,6 +107,7 @@ impl<T: WidgetData> WatcherInit for WidgetInternal<T> {
     }
 }
 
+/// A basic structure to wrap some data and turn it into a widget.
 #[derive(Default)]
 pub struct Widget<T: WidgetData> {
     watcher: Watcher<WidgetInternal<T>>,
@@ -108,10 +128,14 @@ impl<T: WidgetData> Widget<T> {
         renderer.draw(&wid_int.canvas_after);
     }
     
+    /// Get an anonymous reference to this widget. This is required by
+    /// WidgetData::children(), for example.
     pub fn proxy(&self) -> WidgetProxy {
         WidgetProxy { anon: self }
     }
 
+    /// Get an mutable anonymous reference to this widget. This is required
+    /// by WidgetData::children_mut(), for example.
     pub fn proxy_mut(&mut self) -> WidgetProxyMut {
         WidgetProxyMut { anon: self }
     }
@@ -173,11 +197,13 @@ impl<T: WidgetData> AnonWidget for Widget<T> {
     }
 }
 
+/// A proxy to a widget with an unspecified underlying data type.
 #[derive(Copy, Clone)]
 pub struct WidgetProxy<'a> {
     anon: &'a dyn AnonWidget,
 }
 
+/// A mutable proxy to a widget with an unspecified underlying data type.
 pub struct WidgetProxyMut<'a> {
     anon: &'a mut dyn AnonWidget,
 }
