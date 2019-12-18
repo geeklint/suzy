1263fb854eb60379015e36fcf3b81b86534965cb
Violet <violet@geeklint.com>
2019-12-17 22:57:18 -0500

    implement dummy window (intended as template for other windows)

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -4,6 +4,11 @@ mod tui;
 #[cfg(feature = "tui")]
 pub use tui::*;
 
+#[cfg(not(any(feature = "tui", )))]
+mod dummy;
+#[cfg(not(any(feature = "tui", )))]
+pub use dummy::*;
+
 pub enum WindowEvent {
     Resize(f32, f32),
     KeyDown(i32),
