c325afe2603f2b2f53129337307c991d0d88f2f9
Violet <violet@geeklint.com>
2020-04-06 18:42:27 -0400

    implement using kerning to render fonts

--- src/platform/opengl/text.rs
+++ src/platform/opengl/text.rs
@@ -5,16 +5,18 @@ use super::graphics::primitive::{Buffer, TextureBuilder};
 
 type Coord = (u32, f32, f32, f32, f32);
 
-pub struct FontSource<'a, 'b> {
+pub struct FontSource<'a, 'b, 'c> {
     pub atlas_image: &'b [u8],
     pub coords: &'a [Coord],
     pub font_size: f32,
     pub image_width: GLsizei,
     pub image_height: GLsizei,
+    pub padding_ratio: f32,
+    pub kerning_pairs: &'c [(char, char, f32)],
 }
 
-impl<'a, 'b> FontSource<'a, 'b> {
-    pub fn load(&self) -> Font<'a> {
+impl<'a, 'b, 'c> FontSource<'a, 'b, 'c> {
+    pub fn load(&self) -> Font<'a, 'c> {
         let mut builder = TextureBuilder::create_custom(
             gl::R8,
             self.image_width,
@@ -30,19 +32,21 @@ impl<'a, 'b> FontSource<'a, 'b> {
         Font {
             texture,
             coords: self.coords,
-            font_size: self.font_size,
+            font_size: self.font_size / (self.image_height as f32),
+            padding_ratio: self.padding_ratio,
+            kerning_pairs: self.kerning_pairs,
+            space_ratio: 0.25,
         }
     }
 }
 
-pub struct Font<'a> {
+pub struct Font<'a, 'b> {
     texture: Texture,
     coords: &'a [Coord],
     font_size: f32,
-}
-
-impl Font<'_> {
-    
+    padding_ratio: f32,
+    kerning_pairs: &'b [(char, char, f32)],
+    space_ratio: f32,
 }
 
 pub struct Text {
@@ -63,14 +67,18 @@ impl Text {
     pub fn render(
         &mut self,
         text: &str,
-        font: &Font<'_>,
+        font: &Font<'_, '_>,
         font_size: f32,
     ) {
         self.texture = font.texture.clone();
         let mut coord_data = vec![];
         let mut uv_data = vec![];
-        let mut x_offset = 0.0;
-        for ch in text.chars() {
+        let mut iter = text.chars().peekable();
+        let render_size = font_size * font.padding_ratio;
+        let offset = (render_size - font_size) / -2.0;
+        let mut x_offset = offset;
+        let padding_amount = font.font_size / font.padding_ratio;
+        while let Some(ch) = iter.next() {
             if let Ok(index) = font.coords.binary_search_by_key(
                 &(ch as u32),
                 |coord| coord.0
@@ -90,28 +98,41 @@ impl Text {
                 uv_data.push(y + height);
                 uv_data.push(x + width);
                 uv_data.push(y);
-                let width = width / height * font_size;
-                let height = font_size;
+                let width_unpadded = width - padding_amount;
+                let height_unpadded = height - padding_amount;
+                let ch_width = font_size * width_unpadded / height_unpadded;
+                let width = render_size * width / height;
+                let height = render_size;
                 let x = x_offset;
-                x_offset += width;
+                if let Some(next) = iter.peek() {
+                    if let Ok(index) = font.kerning_pairs.binary_search_by_key(
+                        &(ch, *next),
+                        |v| (v.0, v.1),
+                    ) {
+                        let kern_ratio = font.kerning_pairs[index].2;
+                        x_offset += ch_width * kern_ratio;
+                    } else {
+                        x_offset += ch_width;
+                    }
+                }
                 // top-left triangle
                 coord_data.push(x);
-                coord_data.push(0.0);
+                coord_data.push(offset);
                 coord_data.push(x + width);
-                coord_data.push(height);
+                coord_data.push(offset + height);
                 coord_data.push(x);
-                coord_data.push(height);
+                coord_data.push(offset + height);
                 // bottom right triangle
                 coord_data.push(x);
-                coord_data.push(0.0);
+                coord_data.push(offset);
                 coord_data.push(x + width);
-                coord_data.push(0.0);
+                coord_data.push(offset);
                 coord_data.push(x + width);
-                coord_data.push(height);
+                coord_data.push(offset + height);
+            } else if ch == ' ' {
+                x_offset += font_size * font.space_ratio;
             }
         }
-        println!("{:?}", &coord_data);
-        println!("{:?}", &uv_data);
         self.coords.set_data(&coord_data);
         self.uvs.set_data(&uv_data);
     }
