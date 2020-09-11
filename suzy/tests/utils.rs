528bb803fbfed9ec2fcae7933531f5d730abfa65
Violet <violet@geeklint.com>
2020-09-11 19:58:28 -0400

    add MPL header to test and example files

--- suzy/tests/utils.rs
+++ suzy/tests/utils.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 use suzy::math::Color;
 
 // use this to ensure we split at pixel boundries (4 bytes)
