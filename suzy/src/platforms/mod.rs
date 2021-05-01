340e701b95ef8762a872d4948d365bfc66b4f776
Violet <violet@geeklint.com>
2021-05-01 10:41:04 -0400

    rename sdl feature to platform_sdl, clean up the way the platform selection logic works

--- suzy/src/platforms/mod.rs
+++ suzy/src/platforms/mod.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 //! This module contains the built-in implementations of the Suzy platform.
 
 // stub types are used in case no other defaults are available - all their
@@ -7,65 +11,19 @@ mod stub;
 #[cfg(feature = "platform_opengl")]
 pub mod opengl;
 
-#[cfg(feature = "sdl")]
+#[cfg(feature = "platform_sdl")]
 pub mod sdl2;
 
-#[cfg(any(
-    feature = "platform_osmesa",
-    all(test, feature = "platform_osmesa_test"),
-))]
+#[cfg(feature = "platform_osmesa")]
 pub mod osmesa;
 
-// Default Platform
-
 /// The default Platform is determined by the cargo features enabled on
 /// this crate.
-#[cfg(not(feature = "platform_opengl"))]
-pub use self::stub::StubPlatform as DefaultPlatform;
-
-/// The default Platform is determined by the cargo features enabled on
-/// this crate.
-#[cfg(all(feature = "platform_opengl", not(feature = "sdl")))]
-pub use self::stub::StubOpenglPlatform as DefaultPlatform;
-
-/// The default Platform is determined by the cargo features enabled on
-/// this crate.
-#[cfg(feature = "sdl")]
-pub use self::sdl2::SdlPlatform as DefaultPlatform;
-
-// Platform used for tests
+pub type DefaultPlatform = _DefaultPlatform;
 
 /// The default Platform used for tests is determined by the cargo features
 /// enabled on this crate.
-#[cfg(not(feature = "platform_opengl"))]
-pub use self::stub::StubPlatform as TestPlatform;
-
-/// The default Platform used for tests is determined by the cargo features
-/// enabled on this crate.
-#[cfg(all(
-    feature = "platform_opengl",
-    not(feature = "sdl"),
-    not(feature = "platform_osmesa"),
-    not(all(test, feature = "platform_osmesa_test")),
-))]
-pub use self::stub::StubOpenglPlatform as TestPlatform;
-
-/// The default Platform used for tests is determined by the cargo features
-/// enabled on this crate.
-#[cfg(all(
-    feature = "sdl",
-    not(feature = "platform_osmesa"),
-    not(all(test, feature = "platform_osmesa_test")),
-))]
-pub use self::sdl2::SdlPlatform as TestPlatform;
-
-/// The default Platform used for tests is determined by the cargo features
-/// enabled on this crate.
-#[cfg(any(
-    feature = "platform_osmesa",
-    all(test, feature = "platform_osmesa_test"),
-))]
-pub use self::osmesa::OsMesaPlatform as TestPlatform;
+pub type TestPlatform = _TestPlatform;
 
 /// The default RenderPlatform is determined by the cargo features enabled on
 /// this crate.
@@ -76,3 +34,39 @@ pub type DefaultRenderPlatform =
 /// features enabled on this crate.
 pub type TestRenderPlatform =
     <TestPlatform as crate::platform::Platform>::Renderer;
+
+// Determe DefaultPlatform by checking cargo features
+
+#[cfg(feature = "platform_sdl")]
+use self::sdl2::SdlPlatform as _DefaultPlatform;
+
+#[cfg(all(not(feature = "platform_sdl"), feature = "platform_opengl"))]
+use self::stub::StubOpenglPlatform as _DefaultPlatform;
+
+#[cfg(all(
+    not(feature = "platform_sdl"),
+    not(feature = "platform_opengl")
+))]
+use self::stub::StubPlatform as _DefaultPlatform;
+
+// Determe TestPlatform by checking cargo features
+
+#[cfg(feature = "platform_osmesa")]
+pub use self::osmesa::OsMesaPlatform as _TestPlatform;
+
+#[cfg(all(not(feature = "platform_osmesa"), feature = "platform_sdl",))]
+pub use self::sdl2::SdlPlatform as _TestPlatform;
+
+#[cfg(all(
+    not(feature = "platform_osmesa"),
+    not(feature = "platform_sdl"),
+    feature = "platform_opengl",
+))]
+pub use self::stub::StubOpenglPlatform as _TestPlatform;
+
+#[cfg(all(
+    not(feature = "platform_osmesa"),
+    not(feature = "platform_sdl"),
+    not(feature = "platform_opengl"),
+))]
+pub use self::stub::StubPlatform as _TestPlatform;
